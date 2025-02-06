f893786fc02c1dd00179bcb520882985acd1db70
scott charlesworth <scott@kenja.io>
2025-02-06 11:47:40 +0100

    inital customisations

---
+++ src/components/Business.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { features } from '../constants'
+import styles, { layout } from '../style'
+import Button from './Button'
+
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? 'mb-6' : 'mb-0'} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img
+        src={icon}
+        alt='icon'
+        className='w-[50%] h-[50%] object-contain'
+      />
+    </div>
+    <div className='flex-1 flex flex-col ml-3'>
+      <h4 className='font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1'>
+        {title}
+      </h4>
+      <p className='font-poppins font-normal text-dimWhite text-[16px] leading-[24px]'>
+        {content}
+      </p>
+    </div>
+  </div>
+)
+
+const Business = () => {
+  return (
+    <section id='features' className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Automate your checks, <br className='sm:block hidden'/>With our agent who will handle the process.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Our Permmit Assist Agent, uses LLMs and our contextual map to automate permit checks,
+          improving the speed and effiecncy of your handling process.
+          Generate a report in minutes and use it
+          to approve permits in record time.
+        </p>
+        <Button styles='mt-10'/>
+      </div>
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index}/>
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default Business
