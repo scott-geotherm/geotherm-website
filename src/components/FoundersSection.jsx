e8361ed54efbbb093d35a6f7aca77a51b68f95da
scott charlesworth <scott@kenja.io>
2025-02-28 12:10:10 +0100

    founders image seperated

--- src/components/FoundersSection.jsx
+++ src/components/FoundersSection.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from '../style';
 import founderImage from '../assets/founders.jpg'; // Replace with the actual path to your image
+import founderImage1 from '../assets/scott.jpg'; // Replace with the actual path to your image
+import founderImage2 from '../assets/alexander.jpg'; // Replace with the actual path to your image
 
 const FoundersSection = () => {
   return (
@@ -9,8 +11,29 @@ const FoundersSection = () => {
         <h2 className="font-poppins font-bold text-[36px] text-gradient mb-4">
           Meet Our Scientists
         </h2>
+
       </div>
       <div className={`flex flex-col md:flex-row items-center ${styles.flexCenter} xl:px-0 sm:px-16 px-6`}>
+        
+
+      <div className="flex-1 flex justify-center my-10 md:my-0">
+          <img src={founderImage1} alt="Founders" className="w-[200px] h-[200px] object-cover rounded-full" />
+        </div>
+
+        <div className="flex-1 flex flex-col items-center md:items-start md:pl-10">
+          <h3 className="font-poppins font-semibold text-[24px] text-white mb-4">Scott Charlesworth, PhD.</h3>
+          <p className={`${styles.paragraph} max-w-[400px] text-center md:text-left`}>
+            Scott is a visionary AI entrepreneur with a passion for innovation, 
+            technology and a smarter, greener future. 
+            With a background in Data Science and ML Engineering consultancy, 
+            he is now applying his business, science and technical skills to build AI startups.
+          </p>
+        </div>
+
+      <div className="flex-1 flex justify-center my-10 md:my-0">
+          <img src={founderImage2} alt="Founders" className="w-[200px] h-[200px] object-cover rounded-full" />
+        </div>
+        
         <div className="flex-1 flex flex-col items-center md:items-end md:pr-10">
           <h3 className="font-poppins font-semibold text-[24px] text-white mb-4">Alexander Bakker, PhD.</h3>
           <p className={`${styles.paragraph} max-w-[400px] text-center md:text-right`}>
@@ -22,18 +45,11 @@ const FoundersSection = () => {
            
           </p>
         </div>
-        <div className="flex-1 flex justify-center my-10 md:my-0">
-          <img src={founderImage} alt="Founders" className="w-[200px] h-[200px] object-cover rounded-full" />
-        </div>
-        <div className="flex-1 flex flex-col items-center md:items-start md:pl-10">
-          <h3 className="font-poppins font-semibold text-[24px] text-white mb-4">Scott Charlesworth, PhD.</h3>
-          <p className={`${styles.paragraph} max-w-[400px] text-center md:text-left`}>
-            Scott is a visionary AI entrepreneur with a passion for innovation, 
-            technology and a smarter, greener future. 
-            With a background in Data Science and ML Engineering consultancy, 
-            he is now applying his business, science and technical skills to build AI startups.
-          </p>
-        </div>
+
+        
+
+      
+
       </div>
     </section>
   );
